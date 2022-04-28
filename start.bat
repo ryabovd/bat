e0d1f5287fbc5a03c1596ef453d5f30f70a40d8a
ryabovd <ryabovd@outlook.com>
2022-04-28 10:41:33 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,10 @@
+color B
+start "" "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+timeout /t 05
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
+timeout /t 05
+start "" "C:\Users\Алексей\AppData\Local\WhatsApp\WhatsApp.exe"
+timeout /t 05
+start "" "C:\Users\Алексей\AppData\Roaming\Telegram Desktop\Telegram.exe"
+timeout /t 05
+start "" "C:\Users\Алексей\AppData\Local\Viber\Viber.exe"
